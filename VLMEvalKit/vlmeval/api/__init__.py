9f607a37c18c1691f1f454650a6296032e621d65
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2025-08-26 23:03:48 +0100

    modified:   VLMEvalKit/vlmeval/api/__init__.py 	modified:   VLMEvalKit/vlmeval/vlm/__init__.py

--- VLMEvalKit/vlmeval/api/__init__.py
+++ VLMEvalKit/vlmeval/api/__init__.py
@@ -1,24 +1,107 @@
-from .gpt import OpenAIWrapper, GPT4V
-from .hf_chat_model import HFChatModel
-from .gemini import GeminiWrapper, Gemini
-from .qwen_vl_api import QwenVLWrapper, QwenVLAPI, Qwen2VLAPI
-from .qwen_api import QwenAPI
-from .claude import Claude_Wrapper, Claude3V
-from .reka import Reka
-from .glm_vision import GLMVisionAPI
-from .cloudwalk import CWWrapper
-from .sensechat_vision import SenseChatVisionAPI
-from .siliconflow import SiliconFlowAPI, TeleMMAPI
-from .hunyuan import HunyuanVision
-from .bailingmm import bailingMMAPI
-from .bluelm_api import BlueLMWrapper, BlueLM_API
-from .jt_vl_chat import JTVLChatAPI
-from .taiyi import TaiyiAPI
-from .lmdeploy import LMDeployAPI
-from .taichu import TaichuVLAPI, TaichuVLRAPI
-from .doubao_vl_api import DoubaoVL
-from .mug_u import MUGUAPI
-from .kimivl_api import KimiVLAPIWrapper, KimiVLAPI
+try:
+    from .gpt import OpenAIWrapper, GPT4V
+except ImportError:
+    pass
+
+try:
+    from .hf_chat_model import HFChatModel
+except ImportError:
+    pass
+
+try:
+    from .gemini import GeminiWrapper, Gemini
+except ImportError:
+    pass
+
+try:
+    from .qwen_vl_api import QwenVLWrapper, QwenVLAPI, Qwen2VLAPI
+except ImportError:
+    pass
+
+try:
+    from .qwen_api import QwenAPI
+except ImportError:
+    pass
+
+try:
+    from .claude import Claude_Wrapper, Claude3V
+except ImportError:
+    pass
+
+try:
+    from .reka import Reka
+except ImportError:
+    pass
+
+try:
+    from .glm_vision import GLMVisionAPI
+except ImportError:
+    pass
+
+try:
+    from .cloudwalk import CWWrapper
+except ImportError:
+    pass
+
+try:
+    from .sensechat_vision import SenseChatVisionAPI
+except ImportError:
+    pass
+
+try:
+    from .siliconflow import SiliconFlowAPI, TeleMMAPI
+except ImportError:
+    pass
+
+try:
+    from .hunyuan import HunyuanVision
+except ImportError:
+    pass
+
+try:
+    from .bailingmm import bailingMMAPI
+except ImportError:
+    pass
+
+try:
+    from .bluelm_api import BlueLMWrapper, BlueLM_API
+except ImportError:
+    pass
+
+try:
+    from .jt_vl_chat import JTVLChatAPI
+except ImportError:
+    pass
+
+try:
+    from .taiyi import TaiyiAPI
+except ImportError:
+    pass
+
+try:
+    from .lmdeploy import LMDeployAPI
+except ImportError:
+    pass
+
+try:
+    from .taichu import TaichuVLAPI, TaichuVLRAPI
+except ImportError:
+    pass
+
+try:
+    from .doubao_vl_api import DoubaoVL
+except ImportError:
+    pass
+
+try:
+    from .mug_u import MUGUAPI
+except ImportError:
+    pass
+
+try:
+    from .kimivl_api import KimiVLAPIWrapper, KimiVLAPI
+except ImportError:
+    pass
 
 __all__ = [
     'OpenAIWrapper', 'HFChatModel', 'GeminiWrapper', 'GPT4V', 'Gemini',
--- VLMEvalKit/vlmeval/vlm/__init__.py
+++ VLMEvalKit/vlmeval/vlm/__init__.py
@@ -2,111 +2,433 @@ import torch
 
 torch.set_grad_enabled(False)
 torch.manual_seed(1234)
-from .aria import Aria
+
+try:
+    from .aria import Aria
+except ImportError:
+    pass
+
 from .base import BaseModel
-from .hawk_vl import HawkVL
-from .cogvlm import CogVlm, GLM4v
-from .emu import Emu, Emu3_chat, Emu3_gen
-from .eagle_x import Eagle
-from .granite_vision import GraniteVision3
-from .idefics import IDEFICS, IDEFICS2
-from .instructblip import InstructBLIP
-from .kosmos import Kosmos2
-from .llava import (
-    LLaVA,
-    LLaVA_Next,
-    LLaVA_XTuner,
-    LLaVA_Next2,
-    LLaVA_OneVision,
-    LLaVA_OneVision_HF,
-)
-from .vita import VITA, VITAQwen2
-from .long_vita import LongVITA
-from .minicpm_v import MiniCPM_V, MiniCPM_Llama3_V, MiniCPM_V_2_6, MiniCPM_o_2_6
-from .minigpt4 import MiniGPT4
-from .mmalaya import MMAlaya, MMAlaya2
-from .monkey import Monkey, MonkeyChat
-from .moondream import Moondream1, Moondream2
-from .minimonkey import MiniMonkey
-from .mplug_owl2 import mPLUG_Owl2
-from .omnilmm import OmniLMM12B
-from .open_flamingo import OpenFlamingo
-from .pandagpt import PandaGPT
-from .qwen_vl import QwenVL, QwenVLChat
-from .qwen2_vl import Qwen2VLChat, Qwen2VLChatAguvis
-from .transcore_m import TransCoreM
-from .visualglm import VisualGLM
-from .xcomposer import (
-    ShareCaptioner,
-    XComposer,
-    XComposer2,
-    XComposer2_4KHD,
-    XComposer2d5,
-)
-from .yi_vl import Yi_VL
-from .internvl import InternVLChat
-from .deepseek_vl import DeepSeekVL
-from .deepseek_vl2 import DeepSeekVL2
-from .janus import Janus
-from .mgm import Mini_Gemini
-from .bunnyllama3 import BunnyLLama3
-from .vxverse import VXVERSE
-from .gemma import PaliGemma, Gemma3
-from .qh_360vl import QH_360VL
-from .phi3_vision import Phi3Vision, Phi3_5Vision
-from .phi4_multimodal import Phi4Multimodal
-from .wemm import WeMM
-from .cambrian import Cambrian
-from .chameleon import Chameleon
-from .video_llm import (
-    VideoLLaVA,
-    VideoLLaVA_HF,
-    Chatunivi,
-    VideoChatGPT,
-    LLaMAVID,
-    VideoChat2_HD,
-    PLLaVA,
-)
-from .vila import VILA, NVILA
-from .ovis import Ovis, Ovis1_6, Ovis1_6_Plus, Ovis2, OvisU1
-from .mantis import Mantis
-from .mixsense import LLama3Mixsense
-from .parrot import Parrot
-from .omchat import OmChat
-from .rbdash import RBDash
-from .xgen_mm import XGenMM
-from .slime import SliME
-from .mplug_owl3 import mPLUG_Owl3
-from .pixtral import Pixtral
-from .llama_vision import llama_vision
-from .llama4 import llama4
-from .molmo import molmo
-from .points import POINTS, POINTSV15
-from .nvlm import NVLM
-from .vintern_chat import VinternChat
-from .h2ovl_mississippi import H2OVLChat
-from .falcon_vlm import Falcon2VLM
-from .smolvlm import SmolVLM, SmolVLM2
-from .sail_vl import SailVL
-from .valley import Valley2Chat
-from .ross import Ross
-from .ola import Ola
-from .x_vl import X_VL_HF
-from .ursa import UrsaChat
-from .vlm_r1 import VLMR1Chat
-from .aki import AKI
-from .ristretto import Ristretto
-from .vlaa_thinker import VLAAThinkerChat
-from .kimi_vl import KimiVL
-from .wethink_vl import WeThinkVL
-from .flash_vl import FlashVL
-from .oryx import Oryx
-from .treevgr import TreeVGR
-from .glm4_1v import GLM4_1v
-from .varco_vision import VarcoVision
-from .qtunevl import (
-    QTuneVL,
-    QTuneVLChat,
-)
+
+try:
+    from .hawk_vl import HawkVL
+except ImportError:
+    pass
+
+try:
+    from .cogvlm import CogVlm, GLM4v
+except ImportError:
+    pass
+
+try:
+    from .emu import Emu, Emu3_chat, Emu3_gen
+except ImportError:
+    pass
+
+try:
+    from .eagle_x import Eagle
+except ImportError:
+    pass
+
+try:
+    from .granite_vision import GraniteVision3
+except ImportError:
+    pass
+
+try:
+    from .idefics import IDEFICS, IDEFICS2
+except ImportError:
+    pass
+
+try:
+    from .instructblip import InstructBLIP
+except ImportError:
+    pass
+
+try:
+    from .kosmos import Kosmos2
+except ImportError:
+    pass
+
+try:
+    from .llava import (
+        LLaVA,
+        LLaVA_Next,
+        LLaVA_XTuner,
+        LLaVA_Next2,
+        LLaVA_OneVision,
+        LLaVA_OneVision_HF,
+    )
+except ImportError:
+    pass
+
+try:
+    from .vita import VITA, VITAQwen2
+except ImportError:
+    pass
+
+try:
+    from .long_vita import LongVITA
+except ImportError:
+    pass
+
+try:
+    from .minicpm_v import MiniCPM_V, MiniCPM_Llama3_V, MiniCPM_V_2_6, MiniCPM_o_2_6
+except ImportError:
+    pass
+
+try:
+    from .minigpt4 import MiniGPT4
+except ImportError:
+    pass
+
+try:
+    from .mmalaya import MMAlaya, MMAlaya2
+except ImportError:
+    pass
+
+try:
+    from .monkey import Monkey, MonkeyChat
+except ImportError:
+    pass
+
+try:
+    from .moondream import Moondream1, Moondream2
+except ImportError:
+    pass
+
+try:
+    from .minimonkey import MiniMonkey
+except ImportError:
+    pass
+
+try:
+    from .mplug_owl2 import mPLUG_Owl2
+except ImportError:
+    pass
+
+try:
+    from .omnilmm import OmniLMM12B
+except ImportError:
+    pass
+
+try:
+    from .open_flamingo import OpenFlamingo
+except ImportError:
+    pass
+
+try:
+    from .pandagpt import PandaGPT
+except ImportError:
+    pass
+
+try:
+    from .qwen_vl import QwenVL, QwenVLChat
+except ImportError:
+    pass
+
+try:
+    from .qwen2_vl import Qwen2VLChat, Qwen2VLChatAguvis
+except ImportError:
+    pass
+
+try:
+    from .transcore_m import TransCoreM
+except ImportError:
+    pass
+
+try:
+    from .visualglm import VisualGLM
+except ImportError:
+    pass
+
+try:
+    from .xcomposer import (
+        ShareCaptioner,
+        XComposer,
+        XComposer2,
+        XComposer2_4KHD,
+        XComposer2d5,
+    )
+except ImportError:
+    pass
+
+try:
+    from .yi_vl import Yi_VL
+except ImportError:
+    pass
+
+try:
+    from .internvl import InternVLChat
+except ImportError:
+    pass
+
+try:
+    from .deepseek_vl import DeepSeekVL
+except ImportError:
+    pass
+
+try:
+    from .deepseek_vl2 import DeepSeekVL2
+except ImportError:
+    pass
+
+try:
+    from .janus import Janus
+except ImportError:
+    pass
+
+try:
+    from .mgm import Mini_Gemini
+except ImportError:
+    pass
+
+try:
+    from .bunnyllama3 import BunnyLLama3
+except ImportError:
+    pass
+
+try:
+    from .vxverse import VXVERSE
+except ImportError:
+    pass
+
+try:
+    from .gemma import PaliGemma, Gemma3
+except ImportError:
+    pass
+
+try:
+    from .qh_360vl import QH_360VL
+except ImportError:
+    pass
+
+try:
+    from .phi3_vision import Phi3Vision, Phi3_5Vision
+except ImportError:
+    pass
+
+try:
+    from .phi4_multimodal import Phi4Multimodal
+except ImportError:
+    pass
+
+try:
+    from .wemm import WeMM
+except ImportError:
+    pass
+
+try:
+    from .cambrian import Cambrian
+except ImportError:
+    pass
+
+try:
+    from .chameleon import Chameleon
+except ImportError:
+    pass
+
+try:
+    from .video_llm import (
+        VideoLLaVA,
+        VideoLLaVA_HF,
+        Chatunivi,
+        VideoChatGPT,
+        LLaMAVID,
+        VideoChat2_HD,
+        PLLaVA,
+    )
+except ImportError:
+    pass
+
+try:
+    from .vila import VILA, NVILA
+except ImportError:
+    pass
+
+try:
+    from .ovis import Ovis, Ovis1_6, Ovis1_6_Plus, Ovis2, OvisU1
+except ImportError:
+    pass
+
+try:
+    from .mantis import Mantis
+except ImportError:
+    pass
+
+try:
+    from .mixsense import LLama3Mixsense
+except ImportError:
+    pass
+
+try:
+    from .parrot import Parrot
+except ImportError:
+    pass
+
+try:
+    from .omchat import OmChat
+except ImportError:
+    pass
+
+try:
+    from .rbdash import RBDash
+except ImportError:
+    pass
+
+try:
+    from .xgen_mm import XGenMM
+except ImportError:
+    pass
+
+try:
+    from .slime import SliME
+except ImportError:
+    pass
+
+try:
+    from .mplug_owl3 import mPLUG_Owl3
+except ImportError:
+    pass
+
+try:
+    from .pixtral import Pixtral
+except ImportError:
+    pass
+
+try:
+    from .llama_vision import llama_vision
+except ImportError:
+    pass
+
+try:
+    from .llama4 import llama4
+except ImportError:
+    pass
+
+try:
+    from .molmo import molmo
+except ImportError:
+    pass
+
+try:
+    from .points import POINTS, POINTSV15
+except ImportError:
+    pass
+
+try:
+    from .nvlm import NVLM
+except ImportError:
+    pass
+
+try:
+    from .vintern_chat import VinternChat
+except ImportError:
+    pass
+
+try:
+    from .h2ovl_mississippi import H2OVLChat
+except ImportError:
+    pass
+
+try:
+    from .falcon_vlm import Falcon2VLM
+except ImportError:
+    pass
+
+try:
+    from .smolvlm import SmolVLM, SmolVLM2
+except ImportError:
+    pass
+
+try:
+    from .sail_vl import SailVL
+except ImportError:
+    pass
+
+try:
+    from .valley import Valley2Chat
+except ImportError:
+    pass
+
+try:
+    from .ross import Ross
+except ImportError:
+    pass
+
+try:
+    from .ola import Ola
+except ImportError:
+    pass
+
+try:
+    from .x_vl import X_VL_HF
+except ImportError:
+    pass
+
+try:
+    from .ursa import UrsaChat
+except ImportError:
+    pass
+
+try:
+    from .vlm_r1 import VLMR1Chat
+except ImportError:
+    pass
+
+try:
+    from .aki import AKI
+except ImportError:
+    pass
+
+try:
+    from .ristretto import Ristretto
+except ImportError:
+    pass
+
+try:
+    from .vlaa_thinker import VLAAThinkerChat
+except ImportError:
+    pass
+
+try:
+    from .kimi_vl import KimiVL
+except ImportError:
+    pass
+
+try:
+    from .wethink_vl import WeThinkVL
+except ImportError:
+    pass
+
+try:
+    from .flash_vl import FlashVL
+except ImportError:
+    pass
+
+try:
+    from .oryx import Oryx
+except ImportError:
+    pass
+
+try:
+    from .treevgr import TreeVGR
+except ImportError:
+    pass
+
+try:
+    from .glm4_1v import GLM4_1v
+except ImportError:
+    pass
+
+try:
+    from .varco_vision import VarcoVision
+except ImportError:
+    pass
+
+try:
+    from .qtunevl import QTuneVL, QTuneVLChat
+except ImportError:
+    pass
 
 from .socrates_mitm import ZanobiaMitM, from_name
